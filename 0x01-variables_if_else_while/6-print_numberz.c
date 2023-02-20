65124c982b8f5fdc88981ab24798dba06ccec25e
chalalu <majungulawrence@gmail.com>
2023-02-20 15:59:31 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include <time.h>
+/**
+ * main - prints single digit numbers
+ * Return:  Always (Success)
+ *
+ */
+int main(void)
+{
+        int x;
+
+        for (x = 0; x < 10; x++)
+        putchar((x % 10) + '0');
+        putchar('\n');
+        return (0);
+}
